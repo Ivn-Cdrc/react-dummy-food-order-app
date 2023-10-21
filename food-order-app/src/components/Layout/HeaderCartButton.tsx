f93f2be5de51010523365b073317fc67e241101b
Ivn-Cdrc <ivan.cedric.bulosan@gmail.com>
2023-10-22 02:22:14 +1100

    added bump animation to cart on change

--- food-order-app/src/components/Layout/HeaderCartButton.tsx
+++ food-order-app/src/components/Layout/HeaderCartButton.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
@@ -8,14 +8,38 @@ interface HeaderCartButtonProps {
 }
 
 function HeaderCartButton({ onClick }: HeaderCartButtonProps) {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState<boolean>(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((counter, item) => {
+  // use object destructuring to pull items array and isolate it from cartCtx
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((counter, item) => {
     return counter + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ""}`;
+
+  // change btnClasses to include classes.bump and set a timer that removes that class,
+  // so that when it is added again in the future it plays
+  useEffect(() => {
+    if(items.length === 0) {
+      return;
+    }
+    // state changes so component re-evaluates and bump animation is reapplied
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={onClick}>
+    <button className={btnClasses} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
